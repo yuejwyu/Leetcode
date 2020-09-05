f63f72203c83de906edeb140ea752e31ca9daa37
Yue Yu <64169902+yuejwyu@users.noreply.github.com>
2020-09-05 18:19:15 -0400

    Create RestoreIP.java

---
+++ RestoreIP.java
@@ -0,0 +1,35 @@
+package Laicode;
+
+public class RestoreIP {
+  public List<String> Restore(String ip) {
+    // Write your solution here
+    List<String> res = new ArrayList<>();
+    if (ip == null) return res;
+    helper(res, ip, new int[4], 0, 0);
+    return res;
+  }
+
+  private void helper(List<String> res, String ip, int[] cur, int pos, int index) {
+    if (index == ip.length() || pos == 4) {
+      if (index == ip.length() && pos == 4) res.add(parse(cur));
+      return;
+    }
+    if (cur[pos] == 0 && ip.charAt(index) == '0') {
+      helper(res, ip, cur, pos + 1, index + 1);
+    } else {
+      cur[pos] = cur[pos] * 10 + (ip.charAt(index) - '0');
+      if (cur[pos] > 0 && cur[pos] <= 255) {
+        helper(res, ip, cur, pos, index + 1);
+        helper(res, ip, cur, pos + 1, index + 1);
+      }
+      cur[pos] /= 10;
+    }
+  }
+
+  private String parse(int[] cur) {
+    return Integer.toString(cur[0]) + '.' 
+          + Integer.toString(cur[1]) + '.'
+          + Integer.toString(cur[2]) + '.'
+          + Integer.toString(cur[3]);
+  }
+}
